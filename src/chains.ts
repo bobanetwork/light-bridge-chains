333c51639d109ab706166287a5c4311ae453e3cd
wsdt <kevin.riedl@wavect.io>
2024-03-08 21:02:30 +0100

    feat: Initial release

---
+++ src/chains.ts
@@ -0,0 +1,298 @@
+import { ethers } from 'ethers'
+import { ChainInfo, EAirdropSource, IAirdropConfig } from './types'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+
+export type IBobaChain = Omit<ChainInfo, 'chainId' | 'provider'>
+export interface IBobaChains {
+    [chainId: number]: IBobaChain
+}
+
+export enum Asset {
+    ETH = 'ethereum',
+    BOBA = 'boba-network',
+    USDT = 'tether',
+    BNB = 'binancecoin',
+}
+
+/** @dev Sometimes Boba is the native asset. If airdrop is enabled is set on network config level. */
+const DefaultAirdropConfigs: {
+    [asset: string]: Omit<IAirdropConfig, 'airdropEnabled'>
+} = {
+    [Asset.BOBA as string]: {
+        airdropAmountWei: ethers.utils.parseEther('0.5'),
+        airdropCooldownSeconds: 86400, // 1 day
+    },
+    [Asset.ETH as string]: {
+        airdropAmountWei: ethers.utils.parseEther('0.0005'),
+        airdropCooldownSeconds: 86400, // 1 day
+    },
+}
+
+/**
+ * @dev Chain configs
+ * @property supportedAssets: BOBA as fee token only supported for EOAs, since Teleporter consists of a contract & the disburser wallet (assuming ETH fee) everything with 0x0 should be fine.
+ **/
+export const BobaChains: IBobaChains = {
+    // TODO: Consider using AddressManager or AddressPackage instead
+
+    //#region boba_networks
+    288: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_BOBAETHMAINNET ??
+            'https://boba-ethereum.gateway.tenderly.co',
+        testnet: false,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'Boba Ethereum Mainnet',
+        teleportationAddress: '0x0dfFd3Efe9c3237Ad7bf94252296272c96237FF5',
+        height: 1111267,
+        supportedAssets: {
+            ['0xa18bF3994C0Cc6E3b63ac420308E5383f53120D7'.toLowerCase()]: Asset.BOBA,
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: true,
+        },
+    },
+    56288: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_BOBABNBMAINNET ??
+            'https://replica.bnb.boba.network',
+        testnet: false,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'Boba BNB Mainnet',
+        teleportationAddress: '0x670b130112C6f03E17192e63c67866e67D77c3ee',
+        height: 3393,
+        supportedAssets: {
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.BOBA,
+            ['0x4200000000000000000000000000000000000023'.toLowerCase()]: Asset.BNB,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.BOBA],
+            airdropEnabled: true,
+        },
+    },
+    2888: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_BOBAETHGOERLI ??
+            'https://replica.goerli.boba.network',
+        testnet: true,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'Boba Ethereum Goerli',
+        teleportationAddress: '0xB43EE846Aa266228FeABaD1191D6cB2eD9808894',
+        height: 40822,
+        supportedAssets: {
+            ['0x4200000000000000000000000000000000000023'.toLowerCase()]: Asset.BOBA,
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: true,
+        },
+    },
+    9728: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_BOBABNBTESTNET ??
+            'https://boba-bnb-testnet.gateway.tenderly.co',
+        testnet: true,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'Boba BNB Testnet',
+        teleportationAddress: '0xf4d179d3a083Fa3Eede935FaF4C679D32d514186',
+        height: 295353,
+        supportedAssets: {
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.BOBA,
+            ['0x4200000000000000000000000000000000000023'.toLowerCase()]: Asset.BNB,
+            ['0xc614A66f82e71758Fa7735C91dAD1088c8362f15'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.BOBA],
+            airdropEnabled: true,
+        },
+    },
+    421613: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_ARBITRUMGOERLI ??
+            'https://arbitrum-goerli.public.blastapi.io',
+        testnet: true,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'Arbitrum Goerli',
+        teleportationAddress: '0x7063f59e1Db3e505D844d11A71C78F92D39E5963',
+        height: 53880808,
+        supportedAssets: {
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: false,
+        },
+    },
+    420: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_OPTIMISMGOERLI ??
+            'https://optimism-goerli.publicnode.com',
+        testnet: true,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'Optimism Goerli',
+        teleportationAddress: '0xC226F132A686A08018431C913d87693396246024',
+        height: 17010097,
+        supportedAssets: {
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: false,
+        },
+    },
+    42161: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_ARBITRUMMAINNET ??
+            'https://arbitrum.llamarpc.com',
+        testnet: false,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'Arbitrum Mainnet',
+        teleportationAddress: '0x2dE73Bd1660Fbf4D521a52Ec2a91CCc106113801',
+        height: 180755096,
+        supportedAssets: {
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: false,
+        },
+    },
+    10: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_OPTIMISMMAINNET ??
+            'https://optimism.llamarpc.com',
+        testnet: false,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'Optimism Mainnet',
+        teleportationAddress: '0x2dE73Bd1660Fbf4D521a52Ec2a91CCc106113801',
+        height: 116168267,
+        supportedAssets: {
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: false,
+        },
+    },
+    //#endregion
+    //#region l1
+    1: {
+        url: process.env.LIGHTBRIDGE_RPC_ETHMAINNET ?? 'https://eth.llamarpc.com',
+        testnet: false,
+        name: 'Ethereum Mainnet',
+        layer: EAirdropSource.ALLOW,
+        teleportationAddress: '0x2dE73Bd1660Fbf4D521a52Ec2a91CCc106113801',
+        height: 19227141,
+        supportedAssets: {
+            ['0x42bBFa2e77757C645eeaAd1655E0911a7553Efbc'.toLowerCase()]: Asset.BOBA,
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+            ['0xdAC17F958D2ee523a2206206994597C13D831ec7'.toLowerCase()]: Asset.USDT,
+            ['0xB8c77482e45F1F44dE1745F52C74426C631bDD52'.toLowerCase()]: Asset.BNB,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: false,
+        },
+    },
+    56: {
+        url: process.env.LIGHTBRIDGE_RPC_BNBMAINNET ?? 'https://rpc.ankr.com/bsc',
+        testnet: false,
+        name: 'BNB Mainnet',
+        layer: EAirdropSource.PROHIBIT, // also do not allow bnb mainnet as too cheap
+        teleportationAddress: '0x0dfFd3Efe9c3237Ad7bf94252296272c96237FF5',
+        height: 30907682,
+        supportedAssets: {
+            ['0xE0DB679377A0F5Ae2BaE485DE475c9e1d8A4607D'.toLowerCase()]: Asset.BOBA,
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.BNB,
+            ['0x2170Ed0880ac9A755fd29B2688956BD959F933F8'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: false,
+        },
+    },
+    // Sepolia
+    11155111: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_SEPOLIATESTNET ??
+            'https://ethereum-sepolia.publicnode.com',
+        testnet: true,
+        layer: EAirdropSource.ALLOW,
+        name: 'Sepolia Testnet',
+        teleportationAddress: '0xaeE12b8D99BBff7ED47866DF868CF5b4b3F73ffF',
+        height: 5280795,
+        supportedAssets: {
+            ['0x33faF65b3DfcC6A1FccaD4531D9ce518F0FDc896'.toLowerCase()]: Asset.BOBA,
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: false,
+        },
+    },
+    // Boba Sepolia
+    28882: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_BOBASEPOLIATESTNET ??
+            'https://sepolia.boba.network',
+        testnet: true,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'Boba Sepolia Testnet',
+        teleportationAddress: '0x2dE73Bd1660Fbf4D521a52Ec2a91CCc106113801',
+        height: 1043907, // TODO: might need to be adapted
+        supportedAssets: {
+            ['0x4200000000000000000000000000000000000023'.toLowerCase()]: Asset.BOBA,
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: true,
+        },
+    },
+    5: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_GOERLITESTNET ??
+            'https://rpc.ankr.com/eth_goerli',
+        testnet: true,
+        layer: EAirdropSource.ALLOW,
+        name: 'Goerli Testnet',
+        teleportationAddress: '0x84b22166366a6f7E0cD0c3ce9998f2913Bf17A13',
+        height: 9484025,
+        supportedAssets: {
+            ['0xeCCD355862591CBB4bB7E7dD55072070ee3d0fC1'.toLowerCase()]: Asset.BOBA,
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.ETH,
+            ['0xC2C527C0CACF457746Bd31B2a698Fe89de2b6d49'.toLowerCase()]: Asset.USDT,
+            ['0xFC1C82c5EdeB51082CF30FDDb434D2cBDA1f6924'.toLowerCase()]: Asset.BNB,
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: false,
+        },
+    },
+    97: {
+        url:
+            process.env.LIGHTBRIDGE_RPC_BNBTESTNET ??
+            'https://bsc-testnet-rpc.publicnode.com',
+        testnet: true,
+        layer: EAirdropSource.PROHIBIT,
+        name: 'BNB Testnet',
+        teleportationAddress: '0x7f6a32bCaA70c65E08F2f221737612F6fC18347A',
+        height: 32272487,
+        supportedAssets: {
+            ['0x875cD11fDf085e0E11B0EE6b814b6d0b38fA554C'.toLowerCase()]: Asset.BOBA,
+            ['0x0000000000000000000000000000000000000000'.toLowerCase()]: Asset.BNB,
+            ['0xd66c6B4F0be8CE5b39D52E0Fd1344c389929B378'.toLowerCase()]: Asset.ETH, // WETH
+        },
+        airdropConfig: {
+            ...DefaultAirdropConfigs[Asset.ETH],
+            airdropEnabled: false,
+        },
+    },
+    //#endregion
+}
---
+++ src/types.ts
@@ -0,0 +1,90 @@
+import {BigNumber, BigNumberish, Contract, providers} from 'ethers'
+
+export interface IKMSSignerConfig {
+    awsKmsEndpoint: string
+    awsKmsRegion: string
+    awsKmsAccessKey?: string
+    awsKmsSecretKey?: string
+    awsKmsKeyId: string
+    /** @dev Should always be enabled, but can be helpful for debugging and unit tests, .. */
+    disableDisburserCheck?: boolean
+}
+
+export interface SupportedAssets {
+    [address: string]: string // symbol (MUST BE UNIQUE)
+}
+
+export interface AssetReceivedEvent {
+    args: {
+        token: string
+        sourceChainId: BigNumber
+        toChainId: BigNumber
+        depositId: BigNumber
+        emitter: string
+        amount: BigNumber
+    }
+}
+
+/** @dev Allow airdropping gas only when the sourceNetwork is eligible (security check to avoid arbitrage). */
+export enum EAirdropSource {
+    ALLOW = 'allow',
+    PROHIBIT = 'prohibit',
+}
+
+export interface ChainInfo {
+    chainId: number
+    layer: EAirdropSource
+    url: string
+    provider: providers.StaticJsonRpcProvider
+    testnet: boolean
+    name: string
+    teleportationAddress: string
+    height: number
+    supportedAssets: SupportedAssets
+    airdropConfig: IAirdropConfig
+}
+
+export interface DepositTeleportations {
+    Teleportation: Contract
+    chainId: number
+    totalDeposits: BigNumber
+    totalDisbursements: BigNumber
+    height: number
+}
+
+export interface Disbursement {
+    /** @dev Ignored for native disbursements */
+    token: string
+    amount: string
+    addr: string
+    sourceChainId: number | string
+    depositId: number | string
+}
+
+export enum ENetworkMode {
+    TESTNETS = 'testnets',
+    MAINNETS = 'mainnets',
+}
+
+export interface ILightBridgeOpts {
+    rpcUrl: string
+    envModeIsDevelopment: boolean
+    networkMode: ENetworkMode
+    pollingInterval: number
+    blockRangePerPolling: number
+    awsKmsConfig: IKMSSignerConfig
+    localNetworks?: {
+        mainNetwork: ChainInfo
+        selectedBobaNetworks: ChainInfo[]
+    }
+    retryIntervalMs?: number
+}
+
+export interface IAirdropConfig {
+    /** Amount of native gas airdropped to user when conditions are met, also used as threshold */
+    airdropAmountWei?: BigNumberish
+    /** Amount of seconds to wait after previous airdrop */
+    airdropCooldownSeconds?: BigNumberish
+    /** Define if airdrop is enabled on this network */
+    airdropEnabled: boolean
+}
